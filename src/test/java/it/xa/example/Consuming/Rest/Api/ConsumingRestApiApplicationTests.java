6286a91014db39d231dcb9fcb5169d89358ddffd
LiukSkaiWo <albergo.vargroup@gmail.com>
2023-08-22 12:47:30 +0200

    Added blank project to the remote repository

---
+++ src/test/java/it/xa/example/Consuming/Rest/Api/ConsumingRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package it.xa.example.Consuming.Rest.Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
